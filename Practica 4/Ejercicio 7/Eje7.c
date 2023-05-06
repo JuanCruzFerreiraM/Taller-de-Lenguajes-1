58f3d507a55fa8d78cdb28118ecd96181c14a9fe
Juan Cruz Ferreira <juancruzferreiramonteiro04@gmail.com>
2023-05-06 18:07:58 -0300

    Ejercicio 7 terminado

---
+++ Practica 4/Ejercicio 7/Eje7.c
@@ -0,0 +1,160 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*Dudas a la hora de pasar los punteros de a las estructuras, siendo que quiero modificar el valor de los punteros que apuntan a las estructuras, aparentemente debo pasar un puntero que apunte a esos punteros*/
+struct listaEnlazada
+{
+    int dato;
+    struct listaEnlazada *sig;
+};
+
+typedef struct listaEnlazada lista;
+
+void inicializarLista(lista **);
+void eliminarLista(lista **);
+void agregarInicio(lista **, int);
+void agregarFinal(lista **, int);
+int listaLen(lista *);
+void printL(lista *);
+void listaInvert(lista *, lista **);
+
+int main()
+{
+    int elemento;
+    lista *l = NULL;
+    lista *invert = NULL;
+    inicializarLista(&l);
+    printL(l);
+    printf("Ingrese un elemento para agregar al principio: ");
+    scanf("%d", &elemento);
+    agregarInicio(&l, elemento);
+    printL(l);
+    printf("Ingrese un elemento para agregar al final: ");
+    scanf("%d", &elemento);
+    agregarFinal(&l, elemento);
+    printL(l);
+    printf("En la lista hay %d elementos\n", listaLen(l));
+    listaInvert(l, &invert);
+    printL(invert);
+    eliminarLista(&l);
+    eliminarLista(&invert);
+    printL(invert);
+    printL(l);
+    return 0;
+}
+
+void inicializarLista(lista **l)
+{
+    lista *aux;
+    lista *actual = NULL;
+    int elemento;
+    printf("Ingrese un valor entero positivo, para finalizar ingrese 0\n");
+    scanf("%d", &elemento);
+    while (elemento != 0)
+    {
+        if (elemento > 0)
+        {
+            aux = actual;
+            actual = malloc(sizeof(lista));
+            actual->dato = elemento;
+            actual->sig = aux;
+            printf("Ingrese un valor entero positivo, para finalizar ingrese 0\n");
+            scanf("%d", &elemento);
+        }
+        else
+        {
+            printf("El valor ingresado no es un entero positivo, ingrese un valor diferente, para finalizar ingrese 0\n");
+            scanf("%d", &elemento);
+        }
+    }
+    *l = actual;
+}
+
+void eliminarLista(lista **l)
+{
+    lista *aux;
+    while (*l != NULL)
+    {
+        aux = *l;
+        *l = (*l)->sig;
+        free(aux);
+    }
+}
+
+void agregarInicio(lista **l, int n)
+{
+    if (l == NULL)
+    {
+        *l = malloc(sizeof(lista));
+        (*l)->dato = n;
+        (*l)->sig = NULL;
+    }
+    else
+    {
+        lista *aux = *l;
+        *l = malloc(sizeof(lista));
+        (*l)->dato = n;
+        (*l)->sig = aux;
+    }
+}
+
+void agregarFinal(lista **l, int n)
+{
+    lista *nuev = malloc(sizeof(lista));
+    nuev->dato = n;
+    nuev->sig = NULL;
+    lista *act = *l;
+    lista *ant = *l;
+    while (act != NULL)
+    {
+        ant = act;
+        act = act->sig;
+    }
+    if (*l == NULL)
+    {
+        *l = nuev;
+    }
+    else
+    {
+        ant->sig = nuev;
+        nuev->sig = act;
+    }
+}
+
+int listaLen(lista *l)
+{
+    int cant;
+    lista *aux = l;
+    while (aux != NULL)
+    {
+        cant++;
+        aux = aux->sig;
+    }
+    return cant;
+}
+
+void printL(lista *l)
+{
+    lista *aux = l;
+    while (aux != NULL)
+    {
+        printf("%d, ", aux->dato);
+        aux = aux->sig;
+    }
+    printf("\n");
+    if (l == NULL)
+    {
+        printf("La lista se encuentra vacia");
+    }
+}
+
+void listaInvert(lista *original, lista **invert)
+{
+    lista *aux = original;
+    while (aux != NULL)
+    {
+        agregarInicio(invert, aux->dato);
+        aux = aux->sig;
+    }
+}
